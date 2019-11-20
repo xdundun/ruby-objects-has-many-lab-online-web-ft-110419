e8c8c8f2472c4c2519129037f537443e007aad9e
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 15:05:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,15 +8,17 @@ class Artist
     @songs = []
   end
 
+  def songs
+      @songs
+  end
+
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
 
-  def songs
-      @songs
-  end
+
 
   def self.song_count
     @@song_count
