13f3a8235748f6e3734a972b959af4b99310d181
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 15:00:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ class Artist
   attr_accessor :name
 
   @@song_count = 0
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -10,6 +11,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def songs
